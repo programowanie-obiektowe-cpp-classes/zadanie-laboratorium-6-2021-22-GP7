d7c1f541918d3f8c80acb28b33bdc1f3b586c26c
gpela <grzegorzpela@wp.pl>
2022-01-13 21:37:33 +0100

    16

--- foo.hpp
+++ foo.hpp
@@ -10,12 +10,13 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
     std::for_each(people.begin(), people.end(), [](Human& n) { n.birthday(); });
+    ret_v.clear();
     std::transform(people.rbegin(), people.rend(), std::back_inserter(ret_v), [](Human& h) {
         if (h.isMonster()) {
-            return 'y';
+            return 'n';
         }
         else {
-            return 'n';
+            return 'y';
         };
     });
 
